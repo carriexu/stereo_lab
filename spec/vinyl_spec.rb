0a0c316c7381427afb243d2fd14bbbae39ea3965
graysonthemason <gray.reinhard@gmail.com>
2014-08-19 16:47:38 -0400

    Carrie

--- spec/vinyl_spec.rb
+++ spec/vinyl_spec.rb
@@ -22,7 +22,7 @@ describe(Vinyl) do
     pet_sounds = Vinyl.new('Beach Boys','Pet Sounds', 33)
     expect(pet_sounds.play).to eq("♩♪♫♪ ::'Beach Boys - Pet Sounds' (Side A) plays:: ♪♫♪♩")
   end
-  
+
   it('can be flipped') do
     pet_sounds = Vinyl.new('Beach Boys','Pet Sounds', 33)
     expect(pet_sounds.flip.play).to eq("♩♪♫♪ ::'Beach Boys - Pet Sounds' (Side B) plays:: ♪♫♪♩")
